e8b7cb699e5d079bc9b386b33990872baf18ba26
bgileau <95985118+bgileau@users.noreply.github.com>
2023-02-19 02:15:04 -0700

    Modular + add a search feature (not 100% working)
    searching works, but doesn't include the selection change.

--- MediaArchivalPreselection.js
+++ MediaArchivalPreselection.js
@@ -1,43 +1,71 @@
 // ==UserScript==
 // @name         Media Archival Site Pre-filtering
 // @namespace    http://tampermonkey.net/
-// @version      1.0
+// @version      1.1
 // @description  Preselects a language/type filtering on a media archival site
 // @include      https://nyaa.si/
 // @grant        none
 // ==/UserScript==
 
 (function() {
-    'use strict'
-        setTimeout(function() {
-            const dropdown_highlevel_element = document.getElementById("navFilter-category") // drill down to close to where we want to be
-            const category_button = dropdown_highlevel_element.getElementsByClassName("btn dropdown-toggle btn-default")
+    'use strict';
 
-            // Open the menu (note: click doesn't work, must dispatchevent)
-            category_button[0].dispatchEvent(new MouseEvent('click', {
-                bubbles: true,
-                cancelable: true,
-                view: window
-            }));
-            
-            const select_element = dropdown_highlevel_element.getElementsByClassName("selectpicker show-tick") // drill down a little farther
-            const option_element = select_element[0].querySelector('option[value="1_2"]')
+    function open_category_menu() {
+        const dropdown_highlevel_element = document.getElementById("navFilter-category");
+        const category_button = dropdown_highlevel_element.getElementsByClassName("btn dropdown-toggle btn-default");
 
-            // Set the value of the select element to "option_element" value to select the list item. But, until change event is done, it won't update.
-            select_element[0].value = option_element.value
+        category_button[0].dispatchEvent(new MouseEvent('click', {
+            bubbles: true,
+            cancelable: true,
+            view: window
+        }));
+    }
 
-            // Push the change that we made prior with dispatchevent "change"
-            select_element[0].dispatchEvent(new MouseEvent('change', {
-                bubbles: true,
-                cancelable: true,
-                view: window
-            }));
-            
-            // Close the menu
-            category_button[0].dispatchEvent(new MouseEvent('click', {
-                bubbles: true,
-                cancelable: true,
-                view: window
-            }));
-        }, 1000);
+    function select_category_option() {
+        const dropdown_highlevel_element = document.getElementById("navFilter-category");
+        const select_element = dropdown_highlevel_element.getElementsByClassName("selectpicker show-tick");
+        const option_element = select_element[0].querySelector('option[value="1_2"]');
+
+        select_element[0].value = option_element.value;
+        select_element[0].dispatchEvent(new MouseEvent('change', {
+            bubbles: true,
+            cancelable: true,
+            view: window
+        }));
+    }
+
+    function close_category_menu() {
+        const dropdown_highlevel_element = document.getElementById("navFilter-category");
+        const category_button = dropdown_highlevel_element.getElementsByClassName("btn dropdown-toggle btn-default");
+
+        category_button[0].dispatchEvent(new MouseEvent('click', {
+            bubbles: true,
+            cancelable: true,
+            view: window
+        }));
+    }
+
+    function search(){
+        const search_highlevel_element = document.getElementById("navbar");
+        console.log(search_highlevel_element)
+        const search_button = search_highlevel_element.getElementsByClassName("btn btn-primary")[0];
+        console.log(search_button)
+
+        search_button.dispatchEvent(new MouseEvent("click", {
+            bubbles: true,
+            cancelable: true,
+            view: window
+          }));
+    }
+
+    setTimeout(function() {
+        open_category_menu();
+        select_category_option();
+        close_category_menu();
+        console.log("search");
+        // search();
+        // setTimeout(function() {
+        //     search();
+        // }, 2000);
+    }, 1000);
 })();
